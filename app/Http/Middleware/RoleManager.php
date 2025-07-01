b5b04425fee22a990a7f07a474300da76e0b98ec
Kaleidiana <kaleidiana63@gmail>
2025-07-01 21:01:07 +0300

    Made changes to the roles

--- app/Http/Middleware/RoleManager.php
+++ app/Http/Middleware/RoleManager.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class RoleManager
 {
@@ -13,8 +14,30 @@ class RoleManager
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, $role): Response
     {
-        return $next($request);
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+
+        $authUserRole = Auth::user()->role;
+
+        $roleMap = [
+            'admin' => 0,
+            'worker' => 1,
+            'employer' => 2,
+        ];
+
+        if (isset($roleMap[$role]) && $authUserRole == $roleMap[$role]) {
+            return $next($request);
+        }
+
+        // Redirect to correct dashboard if user tries to access a page for the wrong role
+        return match ($authUserRole) {
+            0 => redirect()->route('admin'),
+            1 => redirect()->route('worker'),
+            2 => redirect()->route('employer'),
+            default => redirect()->route('dashboard'),
+        };
     }
 }
